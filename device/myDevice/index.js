c15bfdf3b3f09289b412031b3a78dda99d833802
Logan Crecraft <logandalec@gmail.com>
2022-08-30 23:01:00 -0700

    fix carriage return

--- device/myDevice/index.js
+++ device/myDevice/index.js
@@ -74,19 +74,68 @@ async function ack_call(self, resolve, reject, cmd) {
     }
 }
 
-//challenge number 2
-async function multi_receive(self, resolve, reject, cmd) {
-    try{
+multi_receive = async function (self, resolve, reject, command, expected) {
+  try {
+    let collected_data = [];
+    let timer = setTimeout(() => {
+      self.parser.removeAllListeners('data');
+      reject(
+        "TrakPod timed out. CMD: " +
+          command.toString().trim() +
+          " failed. Com: " +
+          self.com
+      );
+    }, 15000);
 
-    } catch (err) {
-        reject(err)
-    }
-}
+    let write_ok = await self.port.write(command, function(err) {
+        if(err) reject(err);
+    });
+
+    const promise = new Promise((resolve, reject) => {
+      self.parser.on("data", (data) => {
+        let msg = data.toString("utf8").split(";");
+        let checksum = msg[1].trim();
+        let info = msg[0].split(",");
+        if (!validate_checksum(msg[0], checksum)) {
+          clearTimeout(timer);
+          reject(
+            "Invalid Checksum returned from Pod. received: " +
+              msg[0] +
+              " check: " +
+              checksum
+          );
+        }
+        if (info[0] === "!ACK") {
+          //Good to do nothing...?
+        } else if (info[0] === "!NACK") {
+          self.close();
+          clearTimeout(timer);
+          reject(
+            "Command: " +
+              command.toString().trim() +
+              " not properly acknowledged."
+          );
+        } else if (info[0] === expected) {
+          collected_data.push(msg[0]);
+        }
+        if (msg[0] === self.idle) {
+          clearTimeout(timer);
+          resolve(collected_data);
+        }
+      });
+    });
+    let res = await promise;
+    self.parser.removeAllListeners("data");
+    resolve(res);
+  } catch (err) {
+    reject(err);
+  }
+};
 
 class myDevice {
     constructor(id) {
          this.path = id;
-         this.parser = new ReadlineParser({delimiter: '/r', 
+         this.parser = new ReadlineParser({delimiter: '\r', 
             encoding: 'ascii'});
          this.port = new Serialport({path: id, baudRate: 115200}, 
             function (err) {
@@ -99,7 +148,7 @@ class myDevice {
 
     ledOn() {
         let self = this;
-        let cmd = Buffer.from("!LED,1/r", 'ascii');
+        let cmd = Buffer.from("!LED,1\r", 'ascii');
         return this.queue.add(
             new Promise((resolve, reject) => {
                 ack_call(self, resolve, reject, cmd);
@@ -109,7 +158,7 @@ class myDevice {
 
     ledOff() {
         let self = this;
-        let cmd = Buffer.from("!LED,0/r", 'ascii');
+        let cmd = Buffer.from("!LED,0\r", 'ascii');
         return this.queue.add(() => {
             return new Promise(function (resolve, reject) {
                 ack_call(self, resolve, reject, cmd);
@@ -117,9 +166,6 @@ class myDevice {
         });
     }
 
-    flashNTimes(n) {
-        //Challenge 1
-    }
 
 
 }
